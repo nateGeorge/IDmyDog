2723bab18f0d286567300f9537ee67b35d432e28
wordsforthewise <wordsforthewise@gmail.com>
2016-07-15 17:42:22 -0600

    chore: remove test.js file and add images/ to ignore

--- scrape-ims/test.js
+++
@@ -1,8 +0,0 @@
-var _ = require('underscore');
-
-var ranges = _.range(1, 3)
-
-/*console.log(ranges);
-console.log(ranges.length);
-console.log(ranges[0]);*/
-ranges.forEach(function(e,i,a) {console.log(e)});
