87bdfa17e749452ee51a8616cf1856f9c8287f9a
wordsforthewise <wordsforthewise@gmail.com>
2016-07-19 12:10:16 -0600

    feat: write processed dirs to file

--- scrape-ims/tests/test_get_dir.js
+++ scrape-ims/tests/test_get_dir.js
@@ -16,3 +16,10 @@ dirs.pop('full')
 console.log(dirs)
 
 console.log(dirs.slice(3))
+
+// from here: http://stackoverflow.com/questions/17614123/node-js-how-to-write-an-array-to-file
+// need to keep track of which folders have been used to scrape google images
+var file = fs.createWriteStream('google_processed_dirs.txt');
+file.on('error', function(err) { console.log(err); });
+dirs.forEach(function(v) { file.write(v + '\n'); });
+file.end();
