1526bc290ab98f3a2485d9f6e39655c6347974a0
wordsforthewise <wordsforthewise@gmail.com>
2016-07-19 20:44:25 -0600

    feat: add exploratory analysis of good and bad images

--- process_ims/compare_good_bad.py
+++ process_ims/compare_good_bad.py
@@ -4,9 +4,9 @@ import cv2
 import os
 import pandas as pd
 import pickle
-import pylab as plt
 import mahotas as mh
-from sklearn.svm import LinearSVC, SVC
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-dark')
 
 # load hand-picked good images in 'train' folders 
 try:
@@ -63,3 +63,112 @@ except IOError:
     breed_ims_bad = pd.DataFrame(pdDict)
     
     pickle.dump(breed_ims_bad, open('pickle_files/breed_ims-aff-afg-bad.pd.pk', 'wb'))
+
+def plot_good_n_bad():
+    # get example color histograms of good and bad images
+    
+    # GOOD #1
+    f, axarr = plt.subplots(2, 2)
+    
+    chans = cv2.split(breed_ims_good.images[0])
+    colors = ("b", "g", "r")
+    axarr[0,0].set_title("Color Histogram of good image 1")
+    #axarr[0,0].set_xlabel("Bins")
+    axarr[0,0].set_ylabel("Normalized # of Pixels")
+    stdDevs = []
+    # loop over the image channels
+    for (chan, color) in zip(chans, colors):
+        # create a histogram for the current channel and plot it
+        hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
+        hist /= hist.sum() # normalize to compare images of different sizes
+        stdDevs.append(hist.std())
+        axarr[0,0].plot(hist, color = color)
+        axarr[0,0].set_xlim([0, 256])
+    
+    print('stdev of good img 1:', stdDevs, np.average(stdDevs))
+    
+    # GOOD #2
+    chans = cv2.split(breed_ims_good.images[1])
+    axarr[0,1].set_title("Color Histogram of good image 2")
+    #axarr[0,1].set_xlabel("Bins")
+    #axarr[0,1].set_ylabel("# of Pixels")
+    stdDevs = []
+    # loop over the image channels
+    for (chan, color) in zip(chans, colors):
+        # create a histogram for the current channel and plot it
+        hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
+        hist /= hist.sum() # normalize to compare images of different sizes
+        stdDevs.append(hist.std())
+        axarr[0,1].plot(hist, color = color)
+        axarr[0,1].set_xlim([0, 256])
+    
+    print('stdev of good img 2:', stdDevs, np.average(stdDevs))
+    
+    # BAD #1
+    chans = cv2.split(breed_ims_bad.images[0])
+    axarr[1,0].set_title("'Flattened' Color Histogram of bad image 1")
+    axarr[1,0].set_xlabel("Bins")
+    axarr[1,0].set_ylabel("Normalized # of Pixels")
+    stdDevs = []
+    # loop over the image channels
+    for (chan, color) in zip(chans, colors):
+        # create a histogram for the current channel and plot it
+        hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
+        hist /= hist.sum() # normalize to compare images of different sizes
+        stdDevs.append(hist.std())
+        axarr[1,0].plot(hist, color = color)
+        axarr[1,0].set_xlim([0, 256])
+    
+    print('stdev of bad img 1:', stdDevs, np.average(stdDevs))
+    
+    # BAD #2
+    chans = cv2.split(breed_ims_bad.images[1])
+    axarr[1,1].set_title("'Flattened' Color Histogram of bad image 2")
+    axarr[1,1].set_xlabel("Bins")
+    #axarr[1,1].set_ylabel("# of Pixels")
+    stdDevs = []
+    # loop over the image channels
+    for (chan, color) in zip(chans, colors):
+        # create a histogram for the current channel and plot it
+        hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
+        hist /= hist.sum() # normalize to compare images of different sizes
+        stdDevs.append(hist.std())
+        axarr[1,1].plot(hist, color = color)
+        axarr[1,1].set_xlim([0, 256]) 
+    
+    print('stdev of bad img 2:', stdDevs, np.average(stdDevs))
+    
+    plt.show()
+
+def show_good_n_bad():
+    # show example images used to generate color histograms
+    f, axarr = plt.subplots(2, 2)
+    
+    axarr[0,0].imshow(breed_ims_good.images[0])
+    axarr[0,1].imshow(breed_ims_good.images[1])
+    axarr[1,0].imshow(breed_ims_bad.images[0])
+    axarr[1,1].imshow(breed_ims_bad.images[1])
+    axarr[0,0].set_title('Good image 1')
+    axarr[0,1].set_title('Good image 2')
+    axarr[1,0].set_title('Bad image 1')
+    axarr[1,1].set_title('Bad image 2')
+    plt.show()
+
+def get_color_stds(dataFr):
+    # returns DF of standard deviation of color histograms
+    # takes a pandas dataframe with 'images' column
+    # images should be loaded using cv2.imread()
+    stds = []
+    for im in dataFr.images:
+        chans = cv2.split(im)
+        tmpStd = []
+        for chan in chans:
+            hist = cv2.calcHist([chans[0]], [0], None, [256], [0, 256])
+            hist /= hist.sum() # normalize to compare images of different sizes
+            tmpStd.append(hist.std())
+        stds.append(tmpStd + [np.average(tmpStd)])
+    
+    return pd.DataFrame(stds, columns = ['b', 'g', 'r', 'avg'])
+
+good_stds = get_color_stds(breed_ims_good)
+bad_stds = get_color_stds(breed_ims_bad)
