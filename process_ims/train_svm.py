86ae97fd32fd00b587d15fc16285fc4a7b96a4dd
wordsforthewise <wordsforthewise@gmail.com>
2016-07-27 12:18:26 -0600

    fix: formatting, more parameter space for gridsearch

--- process_ims/train_svm.py
+++ process_ims/train_svm.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import pickle as pk
 from sklearn.svm import SVC
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report
@@ -8,8 +10,6 @@ pcaFG = pk.load(open('pickle_files/pcaFG.pk', 'rb'))
 # load training data
 training_data = pk.load(open('pickle_files/training_data.pd.pk', 'rb'))
 
-#C = 
-
 Tdata = training_data.drop('breed', 1)
 data = []
 for i in range(Tdata.shape[0]):
@@ -18,17 +18,27 @@ for i in range(Tdata.shape[0]):
 
 labels = training_data.breed
 
-X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.5, stratify=labels, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.33, stratify=labels, random_state=42)
 
 clf = SVC(random_state=42)
-params = {"C": [0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]}
+params = {
+          'kernel': ['rbf', 'sigmoid'],
+          'C': [0.05, 0.1, 0.2, 0.5, 1.0, 2.0],
+          'gamma': ['auto', 1, 1/2.]
+         }
 model = GridSearchCV(clf, params, cv=3)
 model.fit(data, labels)
 print("[INFO] best hyperparameters: {}".format(model.best_params_))
+model.fit(X_train, y_train)
 
 model.score(X_test, y_test)
 
 predictions = model.predict(X_test)
+
+score = 0
+for i in range(predictions.shape[0]):
+    if predictions[i]==y_test.iloc[i]:
+        score+=1
 
 print(classification_report(y_test, predictions))
 
