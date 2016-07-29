3bfa217dba18975ccbaf6427e6e6c87ab398dcd0
wordsforthewise <wordsforthewise@gmail.com>
2016-07-29 12:23:32 -0600

    chore: clean up unnecessary files

--- scrape_ims/didnt-work/test_im_scrape.js
+++
@@ -1,6 +0,0 @@
-var Scraper = require('image-scraper');
-var scraper = new Scraper('https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#safe=off&q=dog+breeds&stick=H4sIAAAAAAAAAONgFuLUz9U3SCqxzClSQjC11LKTrfSTMvNz8tMr9VPyc1OLSzKTE0tSU-IT8zJzE3OskopSU1OKHzHGcwu8_HFPWCps0pqT1xgDuIjUKKTGxeaaV5JZUikkw8UrhbBZg0GKmwvB5QEAVcMDHaIAAAA');
- 
-scraper.scrape(function(image) { 
-    image.save();
-});
