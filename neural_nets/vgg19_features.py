239b5cd63fdfdb9971d5fe4b674c74b8390228fa
wordsforthewise <wordsforthewise@gmail.com>
2016-10-06 23:07:16 -0600

    FEAT: extract VGG19 features from dog images

--- neural_nets/vgg19_features.py
+++ neural_nets/vgg19_features.py
@@ -9,6 +9,7 @@ import cPickle as pk
 import os
 import numpy as np
 import pandas as pd
+from time import time
 
 def load_everything():
     # load configuration
@@ -44,17 +45,19 @@ def extract_feats(img_path):
         block4_pool_features = model.predict(x)
         return block4_pool_features
 
-def extract_all_feats():
+def extract_all_feats(timing=True):
     all_feats = None
     for b in breeds:
         print 'on breed:', b
         imDir = mainImPath + breeds[0] + '/'
         ims = os.listdir(imDir)
         for im in ims:
+            start = time()
             print 'on image:', im
             b4 = extract_feats(imDir + im)
             if b4 is None:
                 continue
+
             if all_feats is None:
                 files = [im]
                 all_feats = b4
@@ -62,20 +65,34 @@ def extract_all_feats():
                 files.append(im)
                 all_feats = np.c_[all_feats, all_feats]
 
+            if timing:
+                print 'took', time() - start, 'seconds'
+
     return files, all_feats
 
-def test():
+def test(timing=True):
+    start = time()
     testImDir = mainImPath + breeds[0] + '/'
     testIms = os.listdir(testImDir)
     print 'extracting features for', breeds[0]
     print 'file:', testIms[0]
     b4 = extract_feats(testImDir + testIms[0])
+    if timing:
+        runtime = time() - start
+        print 'took', runtime, 'seconds'
+
+    return b4, runtime
 
 if __name__ == "__main__":
-    # only load if we haven't already (in case we're in ipython)
-    if 'breeds' not in globals():
-        print 'loading data'
-        breeds, bb, mainImPath = load_everything()
+    print 'loading data'
+    breeds, bb, mainImPath = load_everything()
     model = create_model()
-    test()
+    runtest = False
+    if runtest:
+        testFeat, runtime = test()
+        print 'array size (bytes):', testFeat.nbytes
+        print 'total expected size:', (testFeat.nbytes * bb.shape[0]) / 1000000000., 'GB'
+        print 'total run time:', runtime * bb.shape[0] / 60, 'minutes'
+
     files_all_feats = extract_all_feats()
+    pk.dump(files_all_feats, open('files_all_feats.pk', w), 2)
